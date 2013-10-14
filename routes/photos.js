6cee0d71a052c9dff2020f366a1a401dd2acbbf5
marioosh <marioosh.net@gmail.com>
2013-10-14 11:10:57 +0200

    flickr.photos.getSizes out; extras param in flickr.photosets.getPhotos used instead

--- routes/photos.js
+++ routes/photos.js
@@ -38,7 +38,7 @@ var request = require('request');
 }
  */
 var getFiltered = function(req, photoset_id, filter, callback) {
-	var flickr_photosets_getPhotos_url = req.app.get('flickr_api_base_url')+'&photoset_id='+photoset_id+'&method=flickr.photosets.getPhotos'+'&extras=tags'+'&privacy_filter='+filter;
+	var flickr_photosets_getPhotos_url = req.app.get('flickr_api_base_url')+'&photoset_id='+photoset_id+'&method=flickr.photosets.getPhotos'+'&extras=tags,url_m,url_o,url_q'+'&privacy_filter='+filter;
 	
 	/**
 	 * full authorized access
@@ -75,57 +75,13 @@ var getFiltered = function(req, photoset_id, filter, callback) {
 		// callback(error, {photos: photos, photoset: json.photoset});
 		// callback(error, json);
 
-    	async.concat(json.photoset.photo, function(p, callback2){
-    		var flickr_photos_getSizes_url = req.app.get('flickr_api_base_url')+'&user_id='+req.app.get('config').user_id+'&method=flickr.photos.getSizes&photo_id='+p.id;
-    		if(req.app.get('config').auth != null && req.app.get('config').mode != 1) {
-    			flickr_photos_getSizes_url += '&oauth_token='+req.app.get('config').auth.oauth_access_token + '&user_id='+req.app.get('config').auth.results.user_nsid;
-    		}
-    		
-    		request(flickr_photos_getSizes_url, function (error, response, body1){
-    			var json1 = JSON.parse(body1);
-    			var src = {
-    				thumb: '',	// Large Square
-    				full: '',	// Original
-    				lv: '',		// Large
-    				medium: ''	// Medium 
-    			}
-    			var sizes = json1.sizes.size;
-    			for(var i = 0; i < sizes.length; i++) {
-    				var s = sizes[i];
-    				if(s.label == 'Large Square') {
-    					src.thumb = s.source;
-    				}
-    				if(s.label == 'Large') {
-    					src.lv = s.source;
-    				}
-    				if(s.label == 'Original') {
-    					src.full = s.source;
-    				}    				
-    				if(s.label == 'Medium') {
-    					src.medium = s.source;
-    				}    				
-    			}
-    			if(src.lv == ''){
-    				src.lv = src.medium;
-    			}
-    			var cover = {
-    					id: p.id,
-    					title: p.title,
-    					secret: p.secret,
-    					src: src
-    			};
-    			callback2(error, cover);
-    		});    			
-    	}, 
-    	function(err, covers){
-    		callback({
-           		thumbs: covers, 
-           		photoset: {
-           			id: photoset_id,
-           			title: json.photoset.title
-           		}
-           	});
-    	});    		
+		callback({
+       		thumbs: photos, 
+       		photoset: {
+       			id: photoset_id,
+       			title: json.photoset.title
+       		}
+       	});
 	});
 }
 
